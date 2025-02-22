b21d7e8abbea5de5e7515eaded24e4b2a1cf7fe7
Oleg Portnov <drose1@list.ru>
2025-02-22 16:51:13 +0400

    upd package.json -> "@ant-design/v5-patch-for-react-19": "^1.0.3" & App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Layout } from 'antd';
-import { AppHeader } from './components/layout/AppHeader.jsx';
-import { AppSider } from './components/layout/AppSider.jsx';
-import { AppContent } from './components/layout/AppContent.jsx';
-import { CryptoContextProvider } from '../src/context/cryptoContext.jsx'
+import { CryptoContextProvider } from '../src/context/cryptoContext.jsx';
+import { AppLayout } from './components/layout/AppLayout.jsx';
+import '@ant-design/v5-patch-for-react-19';
 
 export default function App() {
 	return (
 		<CryptoContextProvider>
-			<Layout>
-				<AppHeader />
-				<Layout>
-					<AppSider />
-					<AppContent />
-				</Layout>
-			</Layout>
+			<AppLayout />
 		</CryptoContextProvider>
 	);
 }
